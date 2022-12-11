13ae5c4b86b5921deb4e5bf42e86dfdd38604b31
Tejasvi R. Bhagwatkar <trbhagwa@purdue.edu>
2022-12-10 23:28:35 -0500

    ahhhhh

--- tetris(final)/tetris(final).cpp
+++ tetris(final)/tetris(final).cpp
@@ -31,26 +31,38 @@ int figures[7][4] =
 
 bool check()
 {
-    for (int i = 0; i, 4; i++)
-        if (a[i].x, 0 || a[i].x >= N || a[i].y >= M) return 0;
+    for (int i = 0; i < 4; i++)
+        if (a[i].x < 0 || a[i].x >= N || a[i].y >= M) return 0;
         else if (field[a[i].y][a[i].x]) return 0;
+
     return 1;
-}
+};
+
+
 int main()
 {
+    srand(time(0));
+
     RenderWindow window(VideoMode(320, 480), "The Game!");
 
-    Texture t;
-    t.loadFromFile("tiles.png"); 
-    Sprite s(t); 
-    s.setTextureRect(IntRect(0,0,18,18));
+    Texture t1, t2, t3;
+    t1.loadFromFile("tiles.png");
+   
+
+    Sprite s(t1), background(t2), frame(t3);
 
     int dx = 0; bool rotate = 0; int colorNum = 1;
+    float timer = 0, delay = 0.3;
 
-    while
-        (window.isOpen())
+    Clock clock;
+
+    while (window.isOpen())
     {
-        Event e; 
+        float time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        timer += time;
+
+        Event e;
         while (window.pollEvent(e))
         {
             if (e.type == Event::Closed)
@@ -62,11 +74,16 @@ int main()
                 else if (e.key.code == Keyboard::Right) dx = 1;
         }
 
-        for (int i = 0; i < 4; i++) a[i].x += dx;
+        if (Keyboard::isKeyPressed(Keyboard::Down)) delay = 0.15;
 
+        //// <- Move -> ///
+        for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; }
+        if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
+
+        //////Rotate//////
         if (rotate)
         {
-            Point p = a[1];
+            Point p = a[1]; //center of rotation
             for (int i = 0; i < 4; i++)
             {
                 int x = a[i].y - p.y;
@@ -74,28 +91,75 @@ int main()
                 a[i].x = p.x - x;
                 a[i].y = p.y + y;
             }
+            if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
         }
 
-        int n = 3;
-        if (a[0].x==0)
-            for (int i = 0; i < 4; i++)
+        ///////Tick//////
+        if (timer > delay)
+        {
+            for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
+
+            if (!check())
             {
-                a[i].x = figures[n][i] % 2;
-                a[i].y = figures[n][i] / 2;
+                for (int i = 0; i < 4; i++) field[b[i].y][b[i].x] = colorNum;
+
+                colorNum = 1 + rand() % 7;
+                int n = rand() % 7;
+                for (int i = 0; i < 4; i++)
+                {
+                    a[i].x = figures[n][i] % 2;
+                    a[i].y = figures[n][i] / 2;
+                }
             }
 
-        dx = 0; rotate = 0;
+            timer = 0;
+        }
 
+        ///////check lines//////////
+        int k = M - 1;
+        for (int i = M - 1; i > 0; i--)
+        {
+            int count = 0;
+            for (int j = 0; j < N; j++)
+            {
+                if (field[i][j]) count++;
+                field[k][j] = field[i][j];
+            }
+            if (count < N) k--;
+        }
+
+        dx = 0; rotate = 0; delay = 0.3;
+
+        /////////draw//////////
         window.clear(Color::Transparent);
+        window.draw(background);
+
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < N; j++)
+            {
+                if (field[i][j] == 0) continue;
+                s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+                s.setPosition(j * 18, i * 18);
+                s.move(28, 31); //offset
+                window.draw(s);
+            }
+
         for (int i = 0; i < 4; i++)
         {
+            s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
             s.setPosition(a[i].x * 18, a[i].y * 18);
+            s.move(28, 31); //offset
             window.draw(s);
         }
+
+        window.draw(frame);
         window.display();
     }
+
     return 0;
 }
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
